0e25810c193af578c544bb763b2ff52f8d013bb1
zares85 <zares85@gmail.com>
2014-03-08 19:19:34 +0000

    License and minor changes

--- mitele.js
+++ mitele.js
@@ -3,18 +3,23 @@
  *
  *  Copyright (C) 2014 Carlos Jurado
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 (function (plugin) {
     const PREFIX = 'mitele';
@@ -172,11 +177,7 @@
     function getCategoryHTML(category) {
         var url = MITELE_BASEURL ;//+ '/' + category.id;
         showtime.print(url);
-        var html = showtime.httpReq(url).toString();
-        var init = html.indexOf('<div id="submenu_' + category.id);
-        var init = html.indexOf('<ul>', init);
-        var end = html.indexOf('</div>', init);
-        return html.slice(init, end);
+        return showtime.httpReq(url).toString();
     }
 
     /**
@@ -212,7 +213,7 @@
         // Try with descargavideos
         var args = {modo: 1, web: url};
         html = showtime.httpReq(DESCARGAVIDEOS_BASEURL, {args: args}).toString();
-        var ini = html.indexOf('/mitele_handler.php');
+        var ini = html.indexOf('/mitelehandler/');
         var end = Math.min(html.indexOf('"', ini), html.indexOf("'", ini));
         html = html.slice(ini, end);
         return DESCARGAVIDEOS_BASEURL + html;
@@ -229,6 +230,10 @@
      * @returns {Array} programs
      */
     function parsePrograms(html) {
+        var init = html.indexOf('<div id="submenu_' + category.id);
+        init = html.indexOf('<ul>', init);
+        var end = html.indexOf('</div>', init);
+        html = html.slice(init, end);
         html = html.replace(/[\n\r]/g, ' '); // Remove break lines
 
         // Split and parse programs
